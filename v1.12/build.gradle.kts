858ad4c221bee246c685682559ccab15bafef24d
exejar <56853437+exejar@users.noreply.github.com>
2023-07-16 15:27:10 -0400

    forgot to commit

---
+++ v1.12/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.github.weave-mc.weave-gradle") version "bcf6ab0279"
+}
+
+minecraft.version("1.12.2")
+
+dependencies {
+    implementation(project(":shared"))
+}
---
+++ v1.7/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.github.weave-mc.weave-gradle") version "bcf6ab0279"
+}
+
+minecraft.version("1.7.10")
+
+dependencies {
+    implementation(project(":shared"))
+}
